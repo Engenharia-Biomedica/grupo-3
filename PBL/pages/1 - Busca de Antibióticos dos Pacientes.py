9680718792d78bc880b352aa2ba510c4bef1bc8e
luamendess <136856897+luamendess@users.noreply.github.com>
2023-12-07 20:47:35 -0300

    Update 1 - Busca de Antibióticos dos Pacientes.py

--- PBL/pages/1 - Busca de Antibióticos dos Pacientes.py
+++ PBL/pages/1 - Busca de Antibióticos dos Pacientes.py
@@ -2,18 +2,23 @@
 import pandas as pd
 import streamlit as st
 
+#carrgeamento de dados buscando a fonte do arquivo .csv tratado 
 db = pd.read_csv('pbl-tratado.csv', sep=',')
 
+#header 
 st.header("Painel de Monitoramento de Pacientes")
 
+#subheader 
 st.subheader('Tabela interativa de dados', divider='rainbow')
 
+#criação de um filtro de dados 
 def select_box():
     microorganismo = st.selectbox(
         "Antibióticos: ",
-        ['Todos'] + list(db["ds_antibiotico_microorganismo"].unique())
+        ['Todos'] + list(db["ds_antibiotico_microorganismo"].unique()) #seleção da coluna específica do arquivo apenas com variáveis únicas 
     )
 
+    #variável 'todos' irá retornas todos, else retorna apenas variáveis selecionadas 
     if microorganismo == 'Todos':
         db_filt = db
     else:
@@ -26,6 +31,14 @@ def select_box():
     
     if resistencia != 'Todos':
         db_filt = db_filt[db_filt["cd_interpretacao_antibiograma"] == resistencia]
+
+    #filtro para remover as variáveis nulas 
+    db_filt = db_filt.dropna(subset=["ds_antibiotico_microorganismo", "cd_interpretacao_antibiograma"])
+
+    #retorno da tabela 
+    st.dataframe(db_filt) 
+
+select_box()
 
     st.dataframe(db_filt)
 
